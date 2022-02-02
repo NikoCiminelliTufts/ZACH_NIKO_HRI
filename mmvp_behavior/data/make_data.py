8994d4000ff4dc20e2357edf4864270c85ff7d70
zach-at-431556631921 <=>
2022-02-02 10:00:01 -0500

    created the descriptor vector and added concatenation of the descriptor and behavior vectors

--- mmvp_behavior/data/make_data.py
+++ mmvp_behavior/data/make_data.py
@@ -53,8 +53,124 @@ OBJECTS = [
     'egg_plastic_wrap', 'noodle_2', 'timber_semicircle', 'no_object'
 ]
 
-# Objects
 SORTED_OBJECTS = sorted(OBJECTS)
+
+DESCRIPTORS_BY_OBJECT = {
+    "ball_base":["hard","ball","green","small","round","toy"],
+    "ball_basket":["squishy","soft","brown","ball","rubber","round","toy"],
+    "ball_blue":["ball","blue","plastic","hard","round","toy"],
+    "ball_transparent":["ball","blue","transparent","hard","small","round","toy"],
+    "ball_yellow_purple":["ball","yellow","purple","multi-colored","soft","small","round","toy"],
+    "basket_cylinder":["basket","container","wicker","cylindrical","yellow","light","empty"],
+    "basket_funnel":["basket","container","wicker","cylindrical","red","yellow","multi-colored","empty"],
+    "basket_green":["basket","green","container","wicker","empty"],
+    "basket_handle":["basket","brown","container","wicker","handle","empty"],
+    "basket_semicircle":["basket","yellow","container","wicker","empty"],
+    "bigstuffedanimal_bear":["squishy","stuffed animal","bear","brown","soft","big","deformable","toy"],
+    "bigstuffedanimal_bunny":["squishy","stuffed animal","bunny","brown","soft","big","deformable","toy"],
+    "bigstuffedanimal_frog":["squishy","stuffed animal","green","frog","soft","big","deformable","toy"],
+    "bigstuffedanimal_pink_dog":["squishy","stuffed animal","pink","dog","soft","big","deformable","toy"],
+    "bigstuffedanimal_tan_dog":["squishy","stuffed animal","yellow","dog","soft","big","deformable","toy"],
+    "bottle_fuse":["cylindrical","bottle","plastic","empty","container","hard","light","purple"],
+    "bottle_google":["cylindrical","water bottle","bottle","plastic","blue","empty","container","hard","light"],
+    "bottle_green":["cylindrical","bottle","water bottle","empty","plastic","container","green","hard","light"],
+    "bottle_red":["cylindrical","bottle","water bottle","empty","plastic","container","red","squishy","light"],
+    "bottle_sobe":["cylindrical","bottle","purple","plastic","hard","container","empty","light","cylindrical"],
+    "can_arizona":["green","cylindrical","can","metal","aluminum","large","empty","container","open","cylindrical"],
+    "can_coke":["red","cylindrical","can","metal","aluminum","small","empty","container","open","cylindrical"],
+    "can_red_bull_large":["blue","cylindrical","can","metal","aluminum","large","empty","container","open","cylindrical"],
+    "can_red_bull_small":["blue","cylindrical","can","metal","aluminum","small","empty","container","open","cylindrical"],
+    "can_starbucks":["cylindrical","can","metal","aluminum","small","empty","container","open","cylindrical"],
+    "cannedfood_chili":["cylindrical","can","full","metal","multicolored"],
+    "cannedfood_cowboy_cookout":["cylindrical","can","full","metal","multicolored"],
+    "cannedfood_soup":["cylindrical","can","full","metal","multicolored"],
+    "cannedfood_tomato_paste":["cylindrical","can","full","metal","multicolored"],
+    "cannedfood_tomatoes":["cylindrical","can","full","metal","multicolored"],
+    "cone_1":["cone","green","small","short","styrofoam","light"],
+    "cone_2":["cone","green","small","short","styrofoam","light"],
+    "cone_3":["cone","green","medium","styrofoam","light"],
+    "cone_4":["cone","green","tall","styrofoam","light"],
+    "cone_5":["cone","green","tall","big","styrofoam"],
+    "cup_blue":["cup","blue","plastic","empty"],
+    "cup_isu":["cup","red","empty","plastic"],
+    "cup_metal":["cup","metal","empty"],
+    "cup_paper_green":["cup","paper","green","empty"],
+    "cup_yellow":["cup","yellow","plastic","empty"],
+    "egg_cardboard":["egg","green","small","cardboard"],
+    "egg_plastic_wrap":["egg","plastic","small","green"],
+    "egg_rough_styrofoam":["egg","small","styrofoam","green"],
+    "egg_smooth_styrofoam":["egg","small","styrofoam","green"],
+    "egg_wood":["egg","small","wood","green"],
+    "eggcoloringcup_blue":["cup","plastic","small","cylindrical","blue","empty","short","light"],
+    "eggcoloringcup_green":["cup","plastic","small","cylindrical","green","empty","short","light"],
+    "eggcoloringcup_orange":["cup","plastic","small","cylindrical","orange","empty","short","light"],
+    "eggcoloringcup_pink":["cup","plastic","small","cylindrical","pink","empty","short","light"],
+    "eggcoloringcup_yellow":["cup","plastic","small","cylindrical","yellow","empty","short","light"],
+    "medicine_ampicillin":["medicine","container","full","closed","plastic","pills","hard","transparent","orange","short","small"],
+    "medicine_aspirin":["medicine","container","full","closed","plastic","pills","hard","transparent","white","short small"],
+    "medicine_bilberry_extract":["medicine","container","full","closed","plastic","pills","hard","green","short","small"],
+    "medicine_calcium":["medicine","container","full","closed","plastic","pills","hard"," transparent","orange","short","small"],
+    "medicine_flaxseed_oil":["medicine","container","full","closed","plastic","pills","hard","yellow","short","small"],
+    "metal_flower_cylinder":["metal","cylinder","tall","large","empty","container","shiny","closed"],
+    "metal_food_can":["metal","cylinder","short","empty","container","shiny","closed"],
+    "metal_mix_covered_cup":["metal","can","cylinder","empty","open","shiny"],
+    "metal_tea_jar":["metal","can","cylinder","empty","open","shiny"],
+    "metal_thermos":["metal","cylinder","bottle","empty","closed","shiny"],
+    "noodle_1":["pink","foam","soft","deformable","light","short","toy"],
+    "noodle_2":["pink","foam","soft","deformable","light","short","toy"],
+    "noodle_3":["pink","foam","soft","deformable","toy","light"],
+    "noodle_4":["pink","foam","soft","deformable","toy","tall","light"],
+    "noodle_5":["pink","foam","soft","deformable","toy","tall","light","big"],
+    "pasta_cremette":["green","multicolored","small","pasta","box","paper","container","full","closed","deformable","rectangular"],
+    "pasta_macaroni":["blue","multicolored","pasta","box","paper","container","full","closed","deformable","rectangular"],
+    "pasta_penne":["yellow","multicolored","pasta","box","paper","container","full","closed","deformable","large","rectangular"],
+    "pasta_pipette":["blue","multicolored","pasta","box","paper","container","full","closed","deformable","large","rectangular"],
+    "pasta_rotini":["yellow","multicolored","pasta","box","paper","container","full","closed","deformable","large","rectangular"],
+    "pvc_1":["pvc","plastic","cylindrical","round","short","hard","green","pipe","small","light"],
+    "pvc_2":["pvc","plastic","cylindrical","round","short","hard","green","pipe","small"],
+    "pvc_3":["pvc","plastic","cylindrical","round","short","hard","green","pipe"],
+    "pvc_4":["pvc","plastic","cylindrical","round","short","hard","green","pipe","wide"],
+    "pvc_5":["pvc","plastic","cylindrical","round","short","hard","green","pipe","wide"],
+    "smallstuffedanimal_bunny":["squishy","stuffed animal","soft","small","deformable","toy","light","pink"],
+    "smallstuffedanimal_chick":["squishy","stuffed animal","soft","small","deformable","toy","light","green"],
+    "smallstuffedanimal_headband_bear":["squishy","stuffed animal","soft","small","deformable","toy","light","brown"],
+    "smallstuffedanimal_moose":["squishy","stuffed animal","soft","small","deformable","toy","light","brown"],
+    "smallstuffedanimal_otter":["squishy","stuffed animal","soft","small","deformable","toy","light","brown"],
+    "timber_pentagon":["tall","wood","brown","stick","block","hard"],
+    "timber_rectangle":["tall","wood","brown","stick","block","hard"],
+    "timber_semicircle":["tall","wood","brown","stick","block","hard"],
+    "timber_square":["tall","wood","brown","stick","block","hard"],
+    "timber_squiggle":["tall","wood","brown","stick","block","hard"],
+    "tin_pokemon":["box","container","closed","metal","empty","shiny","rectangular","hard","large","tall","yellow","multi-colored"],
+    "tin_poker":["box","container","closed","metal","empty","shiny","rectangular","hard","tall","blue","multicolored"],
+    "tin_snack_depot":["box","container","closed","metal","empty","shiny","rectangular","hard","large","tall","brown","multicolored"],
+    "tin_snowman":["box","container","closed","metal","empty","shiny","rectangular","hard","small","short","blue"],
+    "tin_tea":["box","container","closed","metal","empty","shiny","rectangular","hard","small","short","brown"],
+    "tupperware_coffee_beans":["red","plastic","container","closed","full","hard"],
+    "tupperware_ground_coffee":["red","plastic","container","closed","full","hard"],
+    "tupperware_marbles":["red","plastic","container","closed","full","hard"],
+    "tupperware_pasta":["red","plastic","container","closed","full","hard"],
+    "tupperware_rice":["red","plastic","container","closed","full","hard"],
+    "weight_1":["blue","tall","cylindrical","empty","closed","container","plastic"],
+    "weight_2":["blue","tall","cylindrical","closed","container","plastic"],
+    "weight_3":["blue","tall","cylindrical","full","closed","container","plastic"],
+    "weight_4":["blue","tall","cylindrical","full","closed","container","plastic"],
+    "weight_5":["blue","tall","cylindrical","full","closed","container","plastic"]
+}
+
+DESCRIPTOR_CODES = {'aluminum': 0, 'ball': 1, 'basket': 2, 'bear': 3,
+    'big': 4, 'block': 5, 'blue': 6, 'bottle': 7, 'box': 8, 'brown': 9,
+    'bunny': 10, 'can': 11, 'cardboard': 12, 'closed': 13, 'cone': 14,
+    'container': 15, 'cup': 16, 'cylinder': 17, 'cylindrical': 18, 'deformable': 19,
+    'dog': 20, 'egg': 21, 'empty': 22, 'foam': 23, 'frog': 24, 'full': 25, 'green': 26,
+    'handle': 27, 'hard': 28, 'large': 29, 'light': 30, 'medicine': 31, 'medium': 32,
+    'metal': 33, 'multi-colored': 34, 'multicolor': 35, 'multicolored': 36, 'open': 37,
+    'orange': 38, 'paper': 39, 'pasta': 40, 'pills': 41, 'pink': 42, 'pipe': 43,
+    'plastic': 44, 'purple': 45, 'pvc': 46, 'rectangular': 47, 'red': 48, 'round': 49,
+    'rubber': 50, 'shiny': 51, 'short': 52, 'short, small': 53, 'small': 54, 'soft': 55,
+    'squishy': 56, 'stick': 57, 'stuffed, animal': 58, 'styrofoam': 59, 'tall': 60,
+    'toy': 61, 'transparent': 62, 'water bottle': 63,
+    'white': 64, 'wicker': 65, 'wide': 66, 'wood': 67, 'yellow': 68}
 
 BEHAVIORS = ['crush', 'grasp', 'lift_slow', 'shake', 'poke', 'push', 'tap', 'low_drop', 'hold']
 
@@ -234,6 +350,15 @@ def split(strategy):
     return train_list, test_list
 
 
+# create vector encoding descriptors of an object
+def switch_words_on(object, descriptor_codes, descriptors_by_object):
+    encoded_output = np.zeros(len(descriptor_codes))
+    for descriptor in descriptors_by_object[object]:
+        word_index = descriptor_codes[descriptor]
+        encoded_output[word_index] = 1
+    return encoded_output
+
+
 def process(visions, chosen_behavior, OUT_DIR):
     CHOOSEN_BEHAVIORS = BEHAVIORS
     if chosen_behavior in CHOOSEN_BEHAVIORS:
@@ -270,7 +395,7 @@ def process(visions, chosen_behavior, OUT_DIR):
         behavior = ''
         for _bh in CHOOSEN_BEHAVIORS:
             if _bh in vision.split('/'):# splitting the path in order to allow us to loop over to find the behavior
-		selected_object = vision.split('/')[-5]# The path is object/trial/exec/behavior/file_name
+                selected_object = vision.split('/')[-5]# The path is object/trial/exec/behavior/file_name
                 behavior = _bh
                 save = True
                 break
@@ -302,6 +427,8 @@ def process(visions, chosen_behavior, OUT_DIR):
             continue
         out_behavior_npys = np.zeros(len(CHOOSEN_BEHAVIORS))
         out_behavior_npys[CHOOSEN_BEHAVIORS.index(behavior)] = 1
+        descriptors = switch_words_on(selected_object, DESCRIPTOR_CODES, DESCRIPTORS_BY_OBJECT)
+        out_behavior_npys = np.hstack([out_behavior_npys, descriptors])
 
         for i, (out_vision_npy, out_haptic_npy, out_audio_npy, out_vibro_npy) in enumerate(zip(
                 out_vision_npys, out_haptic_npys, out_audio_npys, out_vibro_npys)):
