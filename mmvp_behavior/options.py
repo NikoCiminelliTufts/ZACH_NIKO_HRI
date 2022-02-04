63fb2f4c393ca7a48b6d016733eaa5c96413e2df
zach-at-431556631921 <=>
2022-02-04 18:36:31 -0500

    adding comments with insights from debugging predictions

--- mmvp_behavior/options.py
+++ mmvp_behavior/options.py
@@ -20,7 +20,7 @@ class Options():
         self.parser.add_argument('--width', type=int, default=64, help='width of image')
         self.parser.add_argument('--output_dir', default='weight', help='directory for model weight.')
         self.parser.add_argument('--pretrained_model', default='', help='filepath of a pretrained model to initialize from.')
-        self.parser.add_argument('--sequence_length', type=int, default=10, help='sequence length, including context frames.')
+        self.parser.add_argument('--sequence_length', type=int, default=10, help='sequence length, including context frames.') # this is specifically for prediction
         self.parser.add_argument('--context_frames', type=int, default=4, help= '# of frames before predictions.')
         self.parser.add_argument('--model', default='CDNA', help='model architecture to use - CDNA, DNA, or STP')
         self.parser.add_argument('--num_masks', type=int, default=10, help='number of masks, usually 1 for DNA, 10 for CDNA, STN.')
@@ -40,7 +40,7 @@ class Options():
         # training details
         self.parser.add_argument('--print_interval', type=int, default=100, help='# iterations to output loss')
         self.parser.add_argument('--schedsamp_k', type=float, default=400, help='The k hyperparameter for scheduled sampling, -1 for no scheduled sampling.')
-        self.parser.add_argument('--batch_size', type=int, default=32, help='batch size for training')
+        self.parser.add_argument('--batch_size', type=int, default=32, help='batch size for training') # minibatch size given to the dataloader as well as pytorch
         self.parser.add_argument('--learning_rate', type=float, default=0.001, help='the base learning rate of the generator')
         self.parser.add_argument('--epochs', type=int, default=30, help='# total training epoch')
         self.opt = None
